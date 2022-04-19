0c59a59b45f4bc5d3cc05cb5ef39bbc97af51b7d
ahmed0tolba <ahmed0waves@gmail.com>
2022-04-20 00:04:25 +0200

    new file:   _database.db 	new file:   app.py 	new file:   identify.py 	new file:   model.model/keras_metadata.pb 	new file:   model.model/saved_model.pb 	new file:   model.model/variables/variables.data-00000-of-00001 	new file:   model.model/variables/variables.index 	new file:   static/images/dental-implant.png 	new file:   static/images/dentist.png 	new file:   static/images/folder.png 	new file:   static/images/img2.png 	new file:   static/images/logo1.png 	new file:   static/images/right-arrow.png 	new file:   static/implants/BW_ahmed_04_19_2022_05_21_06_113_2019.jpg 	new file:   static/implants/BW_ahmed_04_19_2022_21_31_03_113_2019.jpg 	new file:   static/implants/BW_ahmed_04_19_2022_21_32_23_113_2019.jpg 	new file:   static/implants/BW_ahmed_04_19_2022_21_33_13_113_2019.jpg 	new file:   static/implants/ahmed_04_19_2022_05_21_06_113_2019.jpg 	new file:   static/implants/ahmed_04_19_2022_21_31_03_113_2019.jpg 	new file:   static/implants/ahmed_04_19_2022_21_32_23_113_2019.jpg 	new file:   static/implants/ahmed_04_19_2022_21_33_13_113_2019.jpg 	new file:   static/style.css 	new file:   static/styleprofile.css 	new file:   style.css 	new file:   templates/dashboard.html 	new file:   templates/index.html 	new file:   templates/login.html 	new file:   templates/loginbare.html 	new file:   templates/profile.html 	new file:   templates/register.html

---
+++ app.py
@@ -0,0 +1,211 @@
+# pip install flask 
+# pip install Flask-SQLAlchemy flask_login flask_bcrypt flask_wtf wtforms email_validator 
+# 
+# from distutils.command.upload import upload
+import sqlite3
+from flask import Flask,make_response,url_for,redirect, request, render_template,current_app, g, send_file,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager,login_user,login_required,logout_user,current_user
+from flask_wtf import FlaskForm # mistake (wtforms)
+from wtforms import StringField,PasswordField, SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
+from flask_bcrypt import Bcrypt
+##from flask_bootstrap import Bootstrap
+import os
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from identify import getImplantType
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='rahaf'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+databasename = '_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + databasename
+userstablename = "user" 
+images_table_name = "upload" 
+login_manager = LoginManager()    # pip install flask-login 
+login_manager.init_app(app) 
+login_manager.login_view ="login"
+
+##############################
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+########## DB CLASS ###########
+class User(db.Model,UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20),nullable = False,unique = True)
+  email = db.Column(db.String(40),nullable = False,unique = True)
+  password = db.Column(db.String(80),nullable = False)
+
+class Upload(db.Model,UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  imagename = db.Column(db.String(80),nullable = False,unique = True)
+  doctoremail = db.Column(db.String(80),nullable = False,)
+  patientname = db.Column(db.String(20),nullable = False)
+  patientemail = db.Column(db.String(40),nullable = False)
+  comment = db.Column(db.String(40))
+  states = db.Column(db.Integer())
+  manufacturer = db.Column(db.String(40))
+  bw_lvl = db.Column(db.Integer())
+
+########## FLASK FORM  ###########
+class RegisterForm(FlaskForm):
+  username = StringField(validators = [InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Username"})
+  email = StringField(validators = [InputRequired(),Length(min=4,max=40)],render_kw={"placeholder":"Email"})
+  password = PasswordField(validators = [InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+  submit = SubmitField("Register")
+
+  def validate_username(self, username):
+    existing_user_username = User.query.filter_by(username=username.data).first()
+
+    if existing_user_username:
+      raise ValidationError("That user name already exists")
+  
+  def validate_email(self, email):
+    existing_email_email = User.query.filter_by(email=email.data).first()
+
+    if existing_email_email:
+      raise ValidationError("That email name already exists")
+
+########## FLASK FORM  ###########
+class LoginForm(FlaskForm):
+  email = StringField(validators = [InputRequired(),Length(min=4,max=40)],render_kw={"placeholder":"Email"})
+  password = PasswordField(validators = [InputRequired(),Length(min=4,max=20)],render_kw={"placeholder":"Password"})
+  submit = SubmitField("Login")
+
+class UploadForm(FlaskForm):
+  patientname = StringField(validators = [InputRequired(),Length(min=2,max=20)],render_kw={"placeholder":"Name"})
+  patientemail = StringField(validators = [InputRequired(),Length(min=4,max=40)],render_kw={"placeholder":"Email"})
+  comment = StringField(validators = [InputRequired(),Length(min=0,max=40)],render_kw={"placeholder":"Comment"})
+  bw_lvl = StringField(validators = [InputRequired(),Length(min=0,max=3)],render_kw={"placeholder":"BW LVL"})
+
+  submit = SubmitField("Upload")
+
+##################################
+try:
+  print(f'Checking if {databasename} exists or not...')
+  conn = sqlite3.connect(databasename, uri=True)
+  print(f'Database exists. Succesfully connected to {databasename}')
+  conn.execute('CREATE TABLE IF NOT EXISTS ' + userstablename + ' (id INTEGER PRIMARY KEY AUTOINCREMENT,username TEXT UNIQUE NOT NULL,email TEXT UNIQUE NOT NULL,password TEXT NOT NULL, states INTEGER)')
+  conn.execute('CREATE TABLE IF NOT EXISTS ' + images_table_name + ' (id INTEGER PRIMARY KEY AUTOINCREMENT,doctoremail TEXT NOT NULL,patientemail TEXT,patientname TEXT,imagename TEXT NOT NULL, states INTEGER,bw_lvl INTEGER,manufacturer TEXT,comment TEXT)')
+  
+  print(f'Succesfully Created Table {userstablename}')
+  
+  conn.close()
+
+except sqlite3.OperationalError as err:
+  print('Database does not exist')
+  print(err)
+
+
+####### route ###### 
+@app.route('/') 
+def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('dashboard'))
+
+  form1 = LoginForm()
+  if form1.validate_on_submit():
+      # Login and validate the user.
+      # user should be an instance of your `User` class
+      # login_user(user)
+    user = User.query.filter_by(email=form1.email.data).first()
+      # flask.flash('Logged in successfully.')
+    print("hi1")##
+    if user:
+      print("hi")##
+      if bcrypt.check_password_hash(user.password, form1.password.data):
+        login_user(user)
+        return redirect(url_for('dashboard'))
+      # next = flask.request.args.get('next')
+      # # is_safe_url should check if the url is safe for redirects.
+      # # See http://flask.pocoo.org/snippets/62/ for an example.
+      # if not is_safe_url(next):
+      #     return flask.abort(400)
+
+  form2 = RegisterForm()
+  if form2.validate_on_submit():
+    hashed_password = bcrypt.generate_password_hash(form2.password.data)
+    new_user = User(username=form2.username.data,email=form2.email.data,password = hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('login'))
+  return render_template('login.html', form1=form1,form2=form2)
+
+@app.route('/dashboard',methods=['GET','POST'])
+@login_required
+def dashboard():
+  # print(current_user.username)
+  form3 = UploadForm()
+  if form3.validate_on_submit():
+    
+    if request.method == 'POST':
+      # check if the post request has the file part
+      if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+      file = request.files['file']
+      # if user does not select file, browser also
+      # submit a empty part without filename
+      if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+      if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename) 
+        imagename = current_user.username + "_" + datetime.now().strftime("%m_%d_%Y_%H_%M_%S") + "_" +filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], imagename))
+        manufacturer = getImplantType(app.config['UPLOAD_FOLDER'], imagename,int(form3.bw_lvl.data))
+        new_upload = Upload(imagename=imagename,doctoremail=current_user.email, patientname=form3.patientname.data,patientemail=form3.patientemail.data,comment=form3.comment.data,bw_lvl=int(form3.bw_lvl.data),manufacturer=str(manufacturer[0]))
+        db.session.add(new_upload)
+        db.session.commit() 
+  # images = Upload.query.filter_by(email=current_user.email).first()
+  sqlite_insert_query = """select * from upload
+                            where doctoremail = ?;"""
+  data_tuple = (current_user.email,)
+  conn = sqlite3.connect(databasename, uri=True)
+  cur = conn.cursor()
+  cur.execute(sqlite_insert_query,data_tuple)
+  data = cur.fetchall()
+  conn.close()
+  return render_template('dashboard.html',form3=form3,data=data)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+UPLOAD_FOLDER = 'static/implants/'
+ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/logout',methods=['GET','POST'])
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for('login'))
+
+# @app.route('/register',methods=['GET','POST'])
+# def register():
+#   form = RegisterForm()
+#   if form.validate_on_submit():
+#     hashed_password = bcrypt.generate_password_hash(form.password.data)
+#     new_user = User(username=form.username.data,email=form.email.data,password = hashed_password)
+#     db.session.add(new_user)
+#     db.session.commit() 
+
+#     print(f'Succesfully Created User Table {form.email.data}')
+#     return redirect(url_for('login'))
+#   return render_template('register.html',form=form)
+
+if __name__ == '__main__':
+  app.run(debug=True,host="0.0.0.0",use_reloader=True)
+
+#  https://www.sqlite.org/download.html
+#   sqlite3 _database.db 
+# select * from user
---
+++ identify.py
@@ -0,0 +1,214 @@
+# pip install opencv-contrib-python==4.1.2.30 opencv-python==4.1.2.30 tensorflow
+import numpy
+import tensorflow as tf
+import cv2 as cv
+from matplotlib import pyplot as plt
+import math
+from scipy import ndimage
+import numpy as np
+def getImplantType(SaveDir,imgName,thresholdbw):
+    features = getImplantValues(SaveDir,imgName,thresholdbw)
+    print(features)
+    features1 = numpy.array([features[:]])
+    print(features1)
+    loaded_model = tf.keras.models.load_model('model.model')
+    prediction = loaded_model.predict(features1)
+    print(prediction)
+    return prediction
+
+def getImplantValues(SaveDir,imgName,thresholdbw):
+  img =  cv.imread(SaveDir+imgName)[:,:,0]
+  # print("img")
+  # cv2_imshow(img)
+  blured = cv.blur(img,(10,10)) 
+  # print("blured")
+  # cv2_imshow(blured)
+
+  (thresh, BWFilling) = cv.threshold(blured, thresholdbw, 255, cv.THRESH_BINARY)
+  cv.imwrite(SaveDir+"BW_"+imgName, BWFilling)
+  # print("BWFilling")
+  # cv2_imshow(BWFilling)
+
+  # (thresh, BWFillingImplant) = cv.threshold(blured, 150, 255, cv.THRESH_BINARY)
+  # cv2_imshow(BWFillingImplant)
+
+  # contours, hierarchy = cv.findContours(BWFilling,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)[-2:]
+  # idx =0 
+  # for cnt in contours:
+  #     idx += 1
+  #     x,y,w,h = cv.boundingRect(cnt)
+  #     radius = int(h/2 + 10 if h>w else w/2+10)
+  #     BWFillingImplant = cv.circle(BWFillingImplant, (int(x+w/2),int(y+h/2)), radius, (0, 0, 0), -1)
+  # cv2_imshow(BWFilling)
+
+  contours, hierarchy = cv.findContours(BWFilling,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)[-2:]
+  idx = 0
+  idy = 0
+  contours_filter1=[]
+  contours_filter1_box=[]
+  contours_filter1_center=[]
+  contours_filter1_angle=[]
+  dist_from_edge_to_curve=0
+  for cnt in contours:  
+    idx += 1
+    x,y,w,h = cv.boundingRect(cnt)    
+    if ( (w>100 or h >100) and w+h>200):
+      contours_filter1.append(cnt)
+      contours_filter1_box.append([x,y,w,h])
+      contours_filter1_center.append([x+w/2, y+h/2])
+      crop_impant = BWFilling[contours_filter1_box[idy][1]-0:contours_filter1_box[idy][1] + contours_filter1_box[idy][3]+0, contours_filter1_box[idy][0]-0:contours_filter1_box[idy][0]+contours_filter1_box[idy][2]+0]
+      # print("contour number: ", idy)
+      # print("crop_impant")
+      # cv2_imshow(crop_impant)
+      angle = 0
+      Saturated = False
+      
+      max=100
+      c=0
+      direction = 0
+      directionold = 0
+      while (not Saturated):
+        directionold = direction
+        c +=1
+        rotatedPlus = ndimage.rotate(crop_impant, 1+angle)
+        xp,yp,wp,hp = cv.boundingRect(rotatedPlus)
+        rotatedMinus = ndimage.rotate(crop_impant, -1+angle)
+        xm,ym,wm,hm = cv.boundingRect(rotatedMinus)
+        if (wp<wm):
+          angle +=1
+          direction = 1
+        if (wp>wm):
+          angle -=1
+          direction = -1
+        if (c>max or wp==wm or (direction != directionold and direction ==- directionold)):
+          Saturated = True
+          
+      # print("angle ",angle)
+
+      rotated = ndimage.rotate(crop_impant, angle)
+      x,y,w,h = cv.boundingRect(rotated)
+      # print("rotated ")
+      # cv2_imshow(rotated)
+
+      if (w > h):
+        rotated = ndimage.rotate(crop_impant, 90)
+        x,y,w,h = cv.boundingRect(rotated)      
+      
+      
+      crop_rotated_impant = np.zeros((h+10,w+10), np.uint8)
+      crop_rotated_impant[5:y+h+5,5:x+w+5] = rotated[y:y+h, x:x+w]
+      # crop_rotated_impant = rotated[y-5:y+h+5, x-5:x+w+5]
+
+      edges = cv.Canny(crop_rotated_impant,100,200)
+      contours1, hierarchy = cv.findContours(edges,cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)[-2:]
+      # print("edges")
+      # cv2_imshow(edges)
+
+      for contour in contours1:
+        # print("cotlen ", len(contour))
+        if ( len(contour)>200):
+          topleftpoint = []
+          topleftdist = 9999
+          toprightpoint = []
+          toprightdist = 9999
+          bottomleftpoint = []
+          bottomleftdist = 9999
+          bottomrightpoint = []
+          bottomrightdist=9999
+          for point in contour:
+            distthispointtopleft = (((0 - point[0][0] )**2) + ((0-point[0][1])**2) )**0.5
+            distthispointtopright = (((x+w+5 - point[0][0] )**2) + ((0-point[0][1])**2) )**0.5
+            distthispointbottomleft = (((0 - point[0][0] )**2) + ((y+h+5-point[0][1])**2) )**0.5
+            distthispointbottomright = (((x+w+5 - point[0][0] )**2) + ((y+h+5-point[0][1])**2) )**0.5
+            if distthispointtopleft < topleftdist :
+              topleftdist = distthispointtopleft
+              topleftpoint = point[0]
+            if distthispointtopright < toprightdist :
+              toprightdist = distthispointtopright
+              toprightpoint = point[0]
+            if distthispointbottomleft < bottomleftdist :
+              bottomleftdist = distthispointbottomleft
+              bottomleftpoint = point[0]
+            if distthispointbottomright < bottomrightdist :
+              bottomrightdist = distthispointbottomright
+              bottomrightpoint = point[0]
+          
+          dist_up = (((topleftpoint[0] - toprightpoint[0] )**2) + ((topleftpoint[1]-toprightpoint[1])**2) ) ** 0.5
+          dist_down = (((bottomleftpoint[0] - bottomrightpoint[0] )**2) + ((bottomleftpoint[1]-bottomrightpoint[1])**2) ) **0.5
+
+          cv.circle(edges, (topleftpoint[0],topleftpoint[1]), 5, (100, 100, 100), 2)
+          cv.circle(edges, (toprightpoint[0],toprightpoint[1]), 5, (100, 100, 100), 2)
+          cv.circle(edges, (bottomleftpoint[0],bottomleftpoint[1]), 5, (100, 100, 100), 2)
+          cv.circle(edges, (bottomrightpoint[0],bottomrightpoint[1]), 5, (100, 100, 100), 2)
+          print((edges.shape))
+          ninety_percent_dist_to_intersection_point=0
+          flop180 = False
+          if (dist_down + dist_up > w):
+            if dist_up >= dist_down:# and dist_down + dist_up > 100:
+              flop180 = True
+              dist_from_edge_to_curve = ( distthispointbottomleft + distthispointbottomright ) / 2 / w
+              widthratio = dist_down / w
+              ydown = round((bottomleftpoint[1]+bottomrightpoint[1])/2); 
+              widthydown = bottomrightpoint[0]-bottomleftpoint[0]
+              ninety_percent_dist_to_intersection_point = widthydown
+              yup_ninety_percent_dist = ydown
+              while ninety_percent_dist_to_intersection_point < 0.8 * w:
+                ydown -=1
+                firstloc = 0
+                firstrecorded = False
+                secondloc = 0
+                secondrecorded = False
+                count = 0
+                previouspoint=0
+                for point in edges[ydown][:]:
+                  if (count>1):
+                    if (point==255 and firstrecorded and edges[ydown][count-1]==0):
+                      secondrecorded = True
+                      secondloc = count                    
+                      ninety_percent_dist_to_intersection_point = secondloc - firstloc
+                      yup_ninety_percent_dist = ydown
+
+                    if (point==255 and not firstrecorded and edges[ydown][count-1]==0):
+                      firstrecorded = True
+                      firstloc = count                    
+                  previouspoint = point
+                  count += 1
+              edges = cv.line(edges, (0,ydown), (w,ydown), (100, 255, 100), 1)
+
+            else: # dist_up < dist_down
+              dist_from_edge_to_curve = ( distthispointtopleft + distthispointtopright ) / 2 / w
+              widthratio = dist_up / w 
+              yup = round((topleftpoint[1]+toprightpoint[1])/2); 
+              widthyup = toprightpoint[0]-topleftpoint[0]
+              ninety_percent_dist_to_intersection_point = widthyup
+              yup_ninety_percent_dist = yup
+              while ninety_percent_dist_to_intersection_point < 0.8 * w:
+                yup +=1
+                firstloc = 0
+                firstrecorded = False
+                secondloc = 0
+                secondrecorded = False
+                count = 0
+                previouspoint=0
+                for point in edges[yup][:]:
+                  if (count>1):
+                    if (point==255 and firstrecorded and edges[yup][count-1]==0):
+                      secondrecorded = True
+                      secondloc = count                    
+                      ninety_percent_dist_to_intersection_point = secondloc - firstloc
+                      yup_ninety_percent_dist = yup
+
+                    if (point==255 and not firstrecorded and edges[yup][count-1]==0):
+                      firstrecorded = True
+                      firstloc = count                    
+                  previouspoint = point
+                  count += 1
+              edges = cv.line(edges, (0,yup), (w,yup), (100, 255, 100), 1)
+
+          # print("edges")
+          
+        #   cv2_imshow(edges)
+
+      idy +=1
+  print("widthratio",widthratio,"yup_ninety_percent_dist%",yup_ninety_percent_dist/w)
+  return [widthratio, yup_ninety_percent_dist/w]
